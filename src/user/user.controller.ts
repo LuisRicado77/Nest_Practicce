6107b4d9b87d4e956331069776bd10ccc06ed037
LuisRicado77 <luissayas2018@gmail.com>
2024-11-29 11:57:40 -0500

    Adding entities

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { userDto } from 'src/dto/user.dto';
+import { UserUpdateDto } from 'src/dto/user-update.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor(private readonly userService: UserService) {}
+
+  @Get('/:id')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.getUser(id);
+  }
+
+  @Get()
+  getUser() {
+    return this.userService.getUsers();
+  }
+
+  @Post()
+  postUser(@Body() data: userDto) {
+    console.log(data);
+    this.userService.createUser(data);
+    return 'Saved with success';
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: number, @Body() updateUser: UserUpdateDto) {
+    return this.userService.updateUser(id, updateUser);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: number) {
+    return this.userService.deleteUser(id);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,117 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+    Injectable,
+    NotFoundException,
+    RequestTimeoutException,
+    UnprocessableEntityException,
+  } from '@nestjs/common';
+  import { InjectRepository } from '@nestjs/typeorm';
+  import { IUser,IUserCreate,IUserUpdate } from 'src/interfaces/IUser';
+  import { v4 } from 'uuid';
+import { User } from './user.entity';
+  import { Repository } from 'typeorm';
+import { Delete } from '@nestjs/common';
+  
+  @Injectable()
+  export class UserService {
+    private users: IUser[] = [];
+    
+    constructor(
+      @InjectRepository(User) private readonly userRepository: Repository<User>,
+    ) {}
+    async getUsers() {
+      let users: IUser[] | undefined;
+      try {
+        users = await this.userRepository.find();
+      } catch (error) {
+        // Ya me ofrece errores
+        throw new RequestTimeoutException('Error at querying the database', {
+          description: 'Error trying to get users',
+        });
+      }
+  
+      if (!users) {
+        throw new NotFoundException('Not found', {
+          description: 'Users not found',
+        });
+      }
+      return users;
+    }
+  
+    async getUser(id: number) {
+      let user: IUser | undefined;
+      try {
+        user = await this.userRepository.findOne({
+          where: { id },
+          relations: ['tasks'],
+        });
+      } catch (error) {
+        throw new RequestTimeoutException('Error at querying the database', {
+          description: 'Error trying to get user',
+        });
+      }
+      if (!user) {
+        throw new NotFoundException('Not found', {
+          description: 'User not found',
+        });
+      }
+      return user;
+    }
 
-@Injectable()
-export class UserService {}
+  
+    async createUser(user: IUserCreate) {
+      try {
+        const newUser = this.userRepository.create(user);
+        await this.userRepository.save(newUser);
+      } catch (error) {
+        throw new RequestTimeoutException('Error at saving the database', {
+          description: 'Error trying to save user',
+        });
+      }
+    }
+  
+
+
+    async updateUser(id: number, userUpdate: IUserUpdate) {
+      let user: IUser | undefined;
+      try {
+        user = await this.userRepository.findOneBy({ id });
+      } catch (error) {
+        throw new RequestTimeoutException('Error at querying the database', {
+          description: 'Error trying to get user',
+        });
+      }
+      if (!user) {
+        throw new NotFoundException('User not found', {
+          description: 'User does not exists in my app',
+        });
+      }
+  
+      user.age = userUpdate?.age ?? user.age;
+      user.name = userUpdate?.name ?? user.name;
+      user.lastName = userUpdate?.lastName ?? user.lastName;
+  
+      try {
+        await this.userRepository.save(user);
+      } catch (error) {
+        throw new UnprocessableEntityException('Unprocessable error', {
+          description: 'Error at updating user',
+        });
+      }
+      return true
+    }
+  
+   async deleteUser(id: number) {
+      try{
+        const user = await this.userRepository.findOneBy({ id });
+        if(!user){
+          throw new Error('No found it')
+        }
+        this.userRepository.delete(user)
+        return true;
+      }catch(error){
+        throw new Error('No eliminated')
+      }
+      
+    }
+  }
